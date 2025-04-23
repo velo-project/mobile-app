f9f3f31386d7aa4df00474843968ba35addaafdb
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-23 15:29:12 -0300

    wip: user domain implementation

---
+++ Velo.MobileApp.Core/User/Entities/Implementations/UserEntityImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Velo.MobileApp.Core.User.Entities.Exceptions;
+
+namespace Velo.MobileApp.Core.User.Entities.Implementations
+{
+	public sealed class UserEntityImpl : UserEntity
+	{
+		public UserEntityImpl()
+		{
+		}
+
+        public sealed override void ActivateUser()
+        {
+            if (IsBlocked is false)
+                throw new UserAlreadyActivatedException();
+
+            IsBlocked = false;
+        }
+
+        public sealed override void BlockUser()
+        {
+            if (IsBlocked)
+                throw new UserAlreadyBlockedException();
+
+            IsBlocked = true;
+        }
+    }
+}
+
